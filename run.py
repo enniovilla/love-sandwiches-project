b702088b040155f1daa459d1e7c1c2a9088fba0f
Ennio Villa <enniosouzavilla@gmail.com>
2024-04-24 17:54:57 +0000

    Add requirements for deployment

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
 
 
 SCOPE = [
@@ -28,7 +27,7 @@ def get_sales_data():
         print('Data should be six numbers, separated by commas.')
         print('Example: 10,20,30,40,50,60\n')
 
-        data_str = input('Enter your data here: ')
+        data_str = input('Enter your data here:\n')
 
         sales_data = data_str.split(',')
 
@@ -152,6 +151,7 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, 'stock')
+
 
 print('Welcome to Love Sandwiches Data Automation')
 main()
